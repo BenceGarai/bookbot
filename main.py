6b329a2c3bb6f3b989d72f75a74d93ab7910b83a
Bence Garai <garaibence@gmail.com>
2025-05-15 11:16:25 -0700

    Add letter count

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_unique_chars
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -9,8 +10,10 @@ def main():
 
     frankenstein = get_book_text(frankenstein_path)
     num_words = count_words(frankenstein)
+    frankenstein_letter_dict = count_unique_chars(frankenstein)
 
     print(f"{num_words} words found in the document")
+    print (frankenstein_letter_dict)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -4,3 +4,13 @@ def count_words(book):
     for word in words:
         count += 1
     return count
+
+def count_unique_chars(book):
+    letter_dict = {}
+    for letter in book:
+        letter = letter.lower()
+        if letter in letter_dict:
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+    return letter_dict
